985244f8b2f9e2c99270077f953d10a33a0a492a
VDI040 <VDI040@TVDI-040>
2023-06-27 11:14:48 +0530

    First Commit

---
+++ src/test/java/com/api/RestAssure.java
@@ -0,0 +1,14 @@
+package com.api;
+
+import io.restassured.*;
+import io.restassured.specification.RequestSpecification;
+
+public class RestAssure {
+	RequestSpecification reqSpec;
+	public static void main(String[] args) {
+		RestAssured.baseURI = "https://reqres.in/";
+		
+		
+		
+	}
+}
